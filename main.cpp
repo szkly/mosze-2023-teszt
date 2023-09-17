04707546d0b2804fca03452525aa71f3f8d506a5
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-09-17 21:19:08 +0200

    chore: diagnose errors

--- main.cpp
+++ main.cpp
@@ -4,23 +4,37 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+  // TODO: NELEMENTS should be N_ELEMENTS
+  int *b = new int[NELEMENTS];
+
+  // TODO: Add missing semicolon
+  std::cout << '1-100 ertekek duplazasa';
+
+  // TODO: Add missing arguments
+  for (int i = 0;)
+  {
+    b[i] = i * 2;
+  }
+
+  // TODO: Add exit condition
+  for (int i = 0; i; i++)
+  {
+    // TODO: Mising semicolon, value is getting logged
+    std::cout << "Ertek:"
+  }
+
+  std::cout << "Atlag szamitasa: " << std::endl;
+
+  int atlag;
+
+  // TODO: Replace ',' with a semicolon
+  for (int i = 0; i < N_ELEMENTS, i++)
+  {
+    // TODO: Add missing semicolon
+    atlag += b[i]
+  }
+
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  return 0;
 }
